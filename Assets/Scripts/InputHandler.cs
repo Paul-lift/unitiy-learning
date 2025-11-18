98e5686238b951ae25a6d365ab31d3a39967a3d2
Paul-lift <paulbot56@gmail.com>
2025-11-18 23:29:49 +0100

    Initial Commit

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DefaultNamespace{
+    public class InputHandler : MonoBehaviour{
+        public PlayerController PlayerController;
+
+        private InputAction _moveAction, _lookAction, _jumpAction;
+
+        //Event is called once before the first Execution of Update Monobehavoiur is created
+        private void Start(){
+            _moveAction = InputSystem.actions.FindAction("Move");
+            _lookAction = InputSystem.actions.FindAction("Look");
+            _jumpAction = InputSystem.actions.FindAction("Jump");
+
+            _jumpAction.performed += OnJumpPerformed;
+
+            Cursor.visible = false;
+        }
+
+        private void Update(){
+            var movementVector = _moveAction.ReadValue<Vector2>();
+            PlayerController.Move(movementVector);
+
+            var lookVector = _lookAction.ReadValue<Vector2>();
+            PlayerController.Rotate(lookVector);
+        }
+
+        public void OnJumpPerformed(InputAction.CallbackContext ctx){
+            PlayerController.Jump();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace{
+    public class PlayerController : MonoBehaviour{
+        private CharacterController _characterController;
+        
+        public float MovementSpeed = 10f, RotationSpeed = 10f, JumpForce = 10f, GravityForce = -30f;
+        
+        private float _rotationY;
+        private float _verticalVelocity;
+        
+        private void Start(){
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        public void Move(Vector2 movementVector){
+            //transform.forward ist die richtung wo das Objekt hinschaut, Norden z.b (0,0,1), dann wird multipliziert mit 1 oder -1
+            var move = transform.forward * movementVector.y + transform.right * movementVector.x;
+            move = move * (MovementSpeed * Time.deltaTime);
+            _characterController.Move(move);
+            
+            _verticalVelocity = _verticalVelocity + GravityForce * Time.deltaTime;
+            _characterController.Move(new Vector3(0, _verticalVelocity, 0) *  Time.deltaTime);
+        }
+
+        public void Rotate(Vector2 rotationVector){
+            _rotationY += rotationVector.x * RotationSpeed * Time.deltaTime;
+            transform.localRotation = Quaternion.Euler(0, _rotationY, 0);
+        }
+
+        public void Jump(){
+            if (_characterController.isGrounded){
+                _verticalVelocity = JumpForce;
+            }
+        }
+        
+        
+    }
+    
+    
+}
