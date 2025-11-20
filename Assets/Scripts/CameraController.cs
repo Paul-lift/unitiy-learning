4ee795bf9564443d35b023ff82c866b2456a9cc7
Paul-lift <paulbot56@gmail.com>
2025-11-20 16:04:04 +0100

    camera and Playercontroller rework

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
 
 public class CameraController : MonoBehaviour{
+    //Targets
     public Transform FollowTarget, LookTarget;
+    
+    //Follow-Smoothing
+    public float FollowSmoothTime = 0.25f;
+    private Vector3 followVelocity;
+    
+    //Offset
+    public Vector3 Offset = new Vector3(0, 2f, -4f);
 
-    public float FollowSpeed = 10f;
-    // Start is called before the first frame update
+    
     
 
+
     private void LateUpdate(){
-        Vector3 targetPosition = FollowTarget.position;
+        /*var targetPosition = FollowTarget.position;
         transform.position = Vector3.Lerp(transform.position, targetPosition, FollowSpeed * Time.deltaTime);
         transform.LookAt(LookTarget);
+        
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition,)*/
+        
+        Vector3 targetPosition = FollowTarget.position + Offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref followVelocity, FollowSmoothTime);
+        transform.LookAt(LookTarget);
+        
+        
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,40 @@
 
 namespace DefaultNamespace{
     public class PlayerController : MonoBehaviour{
-        private CharacterController _characterController;
-        
         public float MovementSpeed = 10f, RotationSpeed = 10f, JumpForce = 10f, GravityForce = -30f;
-        
+        private CharacterController _characterController;
+
         private float _rotationY;
         private float _verticalVelocity;
-        
+
         private void Start(){
             _characterController = GetComponent<CharacterController>();
         }
 
+        /*  public void Move(Vector2 movementVector){
+              //transform.forward ist die richtung wo das Objekt hinschaut, Norden z.b (0,0,1), dann wird multipliziert mit 1 oder -1
+              var move = transform.forward * movementVector.y + transform.right * movementVector.x;
+              move = move * (MovementSpeed * Time.deltaTime);
+              _characterController.Move(move);
+
+              _verticalVelocity = _verticalVelocity + GravityForce * Time.deltaTime;
+              _characterController.Move(new Vector3(0, _verticalVelocity, 0) *  Time.deltaTime);
+          }*/
+
         public void Move(Vector2 movementVector){
-            //transform.forward ist die richtung wo das Objekt hinschaut, Norden z.b (0,0,1), dann wird multipliziert mit 1 oder -1
-            var move = transform.forward * movementVector.y + transform.right * movementVector.x;
-            move = move * (MovementSpeed * Time.deltaTime);
-            _characterController.Move(move);
+            //transform.forward ist Norden oder süden, alles zwischen 1 und -1 wird mit w(1) oder s(-1) multipliziert und ergibt die richtung des vektor
+            var moveDir = transform.forward * movementVector.y + transform.right * movementVector.x;
+            if (moveDir.sqrMagnitude > 1f) moveDir.Normalize();
             
-            _verticalVelocity = _verticalVelocity + GravityForce * Time.deltaTime;
-            _characterController.Move(new Vector3(0, _verticalVelocity, 0) *  Time.deltaTime);
+            var horizontal = moveDir * MovementSpeed;
+
+
+            if (_characterController.isGrounded && _verticalVelocity < 0) _verticalVelocity = 0f;
+            
+            _verticalVelocity += GravityForce * Time.deltaTime;
+            
+            Vector3 velocity = new Vector3(horizontal.x, _verticalVelocity, horizontal.z);
+            _characterController.Move(velocity * Time.deltaTime);
         }
 
         public void Rotate(Vector2 rotationVector){
@@ -29,13 +44,7 @@ namespace DefaultNamespace{
         }
 
         public void Jump(){
-            if (_characterController.isGrounded){
-                _verticalVelocity = JumpForce;
-            }
+            if (_characterController.isGrounded) _verticalVelocity = JumpForce;
         }
-        
-        
     }
-    
-    
 }
